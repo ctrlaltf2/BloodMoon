4ac4e89060c3facfc66890863e259cd29de8bee7
ctrlaltf2 <ctrlaltf2@protonmail.ch>
2017-11-15 18:48:11 -0500

    Fix mobs spawning in ground

--- src/main/java/uk/co/jacekk/bukkit/bloodmoon/feature/spawning/MoreSpawningListener.java
+++ src/main/java/uk/co/jacekk/bukkit/bloodmoon/feature/spawning/MoreSpawningListener.java
@@ -38,7 +38,7 @@ public final class MoreSpawningListener extends BaseListener<BloodMoon> {
             for (int i = 0; i < Math.max(worldConfig.getInt(Config.FEATURE_MORE_SPAWNING_MULTIPLIER), 1); ++i) {
                 for (BloodMoonEntityType bloodMoonEntity : BloodMoonEntityType.values()) {
                     if (type == bloodMoonEntity.getEntityType()) {
-                        bloodMoonEntity.spawnEntity(event.getLocation().add((random.nextDouble() * 3) - 1.5, (random.nextDouble() * 3) - 1.5, (random.nextDouble() * 3) - 1.5));
+                        bloodMoonEntity.spawnEntity(event.getLocation().add((random.nextDouble() * 3) - 1.5, 1, (random.nextDouble() * 3) - 1.5));
                         return;
                     }
                 }
